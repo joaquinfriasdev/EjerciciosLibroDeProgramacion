12327c45174ae89aaeb2e431d30c4cbc54e23d06
Joaquín Frías <joaquin124507@gmail.com>
2023-08-16 14:40:28 -0300

    Ejercicio Capitulo 1 Ejercicio1

--- Capitulo1Ejercicio1/src/capitulo1ejercicio1/Capitulo1Ejercicio1.java
+++ Capitulo1Ejercicio1/src/capitulo1ejercicio1/Capitulo1Ejercicio1.java
@@ -1,20 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package capitulo1ejercicio1;
 
-/**
- *
- * @author HpRyzen
- */
+import java.util.Scanner;
+
+
 public class Capitulo1Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        double peso, estatura, imc;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Ingrese su peso");
+        peso = teclado.nextDouble();
+        System.out.println("Ingrese su estatura en metros");
+        estatura = teclado.nextDouble();
+        
+        System.out.println("Usted tiene " + peso + " kilogramos y una estatura de " + estatura + " metros");
+        
+        imc = (peso / Math.pow(estatura, 2));
+        
+        if (imc<16) {
+            System.out.println("Usted tiene delgadez severa");
+        } else if (imc < 17) {
+            System.out.println("Usted tiene delgadez moderada");
+        } else if (imc < 18.5) {
+            System.out.println("Usted tiene delgadez leve");
+        } else if (imc < 25) {
+            System.out.println("Usted tiene un peso normal");
+        } else if (imc < 30) {
+            System.out.println("Usted tiene sobrepeso");
+        } else if (imc < 35) {
+            System.out.println("Usted tiene obesidad leve");
+        } else if (imc < 40) {
+            System.out.println("Usted tiene obesidad moderada");
+        } else {
+            System.out.println("Usted tiene obesidad mórbida");
+        }
+    }  
 }
